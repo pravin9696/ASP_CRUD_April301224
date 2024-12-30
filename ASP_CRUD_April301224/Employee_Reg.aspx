b3018621ba2c056c5c7f94bc6149a466db74e299
Acer <Acer@DESKTOP-ABKHEEV>
2024-12-30 13:29:12 +0530

    Add project files.

---
+++ ASP_CRUD_April301224/Employee_Reg.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employee_Reg.aspx.cs" Inherits="ASP_CRUD_April301224.Employee_Reg" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Employee Registration Form</h2>
+            <table>
+                <tr><td>Employee ID</td><td><asp:TextBox ID="txtEmpID" runat="server"></asp:TextBox></td></tr>
+                 <tr><td>Employee Name</td><td><asp:TextBox ID="txtEmpName" runat="server"></asp:TextBox></td></tr>
+                 <tr><td>Contact No.</td><td><asp:TextBox ID="txtContact" runat="server"></asp:TextBox></td></tr>
+                 <tr><td>City</td><td>
+                     <asp:DropDownList ID="dropCity" runat="server" DataSourceID="SqlDataSource2" DataTextField="CityName" DataValueField="CityName">
+                       
+                     </asp:DropDownList>
+                     <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="<%$ ConnectionStrings:EmpDB4AprilConnectionString %>" ProviderName="<%$ ConnectionStrings:EmpDB4AprilConnectionString.ProviderName %>" SelectCommand="SELECT * FROM [tblCity]"></asp:SqlDataSource>
+                     </td></tr>
+                 <tr><td colspan="2">
+                     <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                     <asp:Button ID="btnSearch" runat="server" OnClick="btnSearch_Click" Text="Search" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                     <asp:Button ID="btnUpdate" runat="server" Enabled="False" OnClick="btnUpdate_Click" Text="Update" />
+                     </td></tr>
+
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP_CRUD_April301224/Employee_Reg.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;  //data set
+using System.Data.SqlClient;  // sqlconnection, sqlcommandbuilder, sqldataAdapter
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP_CRUD_April301224
+{
+    public partial class Employee_Reg : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["EmpDB4AprilConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+
+            SqlDataAdapter adp = new SqlDataAdapter("select * from tblEmployee", con);
+            SqlCommandBuilder cmdb = new SqlCommandBuilder(adp);
+            DataSet ds = new DataSet();
+            adp.Fill(ds, "tblEmployee");
+
+            DataRow row = ds.Tables["tblEmployee"].NewRow();
+            row[0] = txtEmpID.Text;
+            row["name"] = txtEmpName.Text;
+            row["contact"] = txtContact.Text;
+            row["cityName"] = dropCity.SelectedValue;
+
+            ds.Tables["tblEmployee"].Rows.Add(row);
+           int n= adp.Update(ds, "tblEmployee");
+            if (n>0)
+            {
+                Response.Write("<script>alert('record inserted successfully');</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('record NOT inserted!!!!!');</script>");
+            }
+
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            /*
+            string constr = ConfigurationManager.ConnectionStrings["EmpDB4AprilConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+
+            SqlDataAdapter adp = new SqlDataAdapter("select * from tblEmployee", con);
+            SqlCommandBuilder cmdb = new SqlCommandBuilder(adp);
+            DataSet ds = new DataSet();
+            adp.Fill(ds, "tblEmployee");
+            int i = 0;
+            bool flag=false;
+            while(i< ds.Tables["tblEmployee"].Rows.Count)
+            {
+                if (string.Compare(ds.Tables["tblEmployee"].Rows[i]["empid"].ToString(),txtEmpID.Text)==0)
+                {
+                    flag = true;
+                    break;
+                }
+                i++;
+            }
+            txtEmpName.Text = ds.Tables["tblEmployee"].Rows[i]["name"].ToString();
+            txtContact.Text= ds.Tables["tblEmployee"].Rows[i]["contact"].ToString();
+            dropCity.Text= ds.Tables["tblEmployee"].Rows[i]["cityName"].ToString();
+            */
+
+            string constr = ConfigurationManager.ConnectionStrings["EmpDB4AprilConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+
+            SqlDataAdapter adp = new SqlDataAdapter("select * from tblEmployee where empid="+txtEmpID.Text, con);
+            SqlCommandBuilder cmdb = new SqlCommandBuilder(adp);
+            DataSet ds = new DataSet();
+            adp.Fill(ds, "tblEmployee");
+            if (ds.Tables[0].Rows.Count==0)
+            {
+                Response.Write("<script>alert('record NOT found!!!!!');</script>");
+                txtEmpName.Text = "";
+                txtContact.Text = "";
+               // dropCity.Text = "";
+                btnUpdate.Enabled = false;
+            }
+            else 
+            {
+                txtEmpName.Text = ds.Tables["tblEmployee"].Rows[0]["name"].ToString();
+                txtContact.Text = ds.Tables["tblEmployee"].Rows[0]["contact"].ToString();
+                dropCity.Text = ds.Tables["tblEmployee"].Rows[0]["cityName"].ToString();
+                btnUpdate.Enabled = true;
+            }
+
+            
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["EmpDB4AprilConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+
+            SqlDataAdapter adp = new SqlDataAdapter("select * from tblEmployee", con);
+            
+            SqlCommandBuilder cmdb = new SqlCommandBuilder(adp);
+            DataSet ds = new DataSet();
+            adp.Fill(ds, "tblEmployee");
+            int i = 0;
+            foreach (DataRow row1 in ds.Tables["tblEmployee"].Rows)
+            {
+                if (row1[0].ToString() == txtEmpID.Text)
+                {
+                    row1[0] = txtEmpID.Text;
+                    row1["name"] = txtEmpName.Text;
+                    row1["contact"] = txtContact.Text;
+                    row1["cityName"] = dropCity.SelectedValue;
+                }
+            }
+           int n= adp.Update(ds, "tblEmployee");
+            if (n > 0)
+            {
+                Response.Write("<script>alert('record updated successfully');</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('record NOT updated!!!!!');</script>");
+            }
+
+        }
+    }
+}
